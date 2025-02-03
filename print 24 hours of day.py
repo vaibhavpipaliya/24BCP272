8061eac1150b92608067c56cebd12b745844c425
vaibhavpipaliya <vaibhavpipaliya79@gmail.com>
2025-02-03 23:28:15 +0530

    Add files via upload

---
+++ print 24 hours of day.py
@@ -0,0 +1,13 @@
+for i in range(24):
+     
+    if i==0:
+     print(i,":midnight")
+    elif i==12:
+     print(i,":noon")
+    elif 1<=i<=11:
+     print(i,":PM")
+    elif 13<=i<=23:
+     print(i,":AM")
+             
+             
+        
